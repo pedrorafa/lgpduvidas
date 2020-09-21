679a362a0754b2ef55241b31ddb86ded1d7e2cf1
Pedro <pedrorafag00@gmail.com>
2020-09-21 20:33:27 -0300

    ajuste do login para VM

---
+++ LgpDuvidas/LgpDuvidas/ViewModels/LoginPageViewModel.cs
@@ -0,0 +1,56 @@
+﻿using LgpDuvidas.Interfaces;
+using LgpDuvidas.Models;
+using System.ComponentModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace LgpDuvidas.ViewModels
+{
+    public class LoginPageViewModel: INotifyPropertyChanged
+    {
+        public IAuthService _authService => DependencyService.Get<IAuthService>();
+        public AuthModel UserModel { get; set; }
+
+        public bool IsLoading { get; set; }
+        public bool HasError { get; set; }
+        public ICommand Login { get; }
+
+        public LoginPageViewModel()
+        {
+            IsLoading = false;
+            HasError = false;
+            UserModel = new AuthModel();
+
+            Login = new Command(OnLogin);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnAppearing()
+        {
+            IsLoading = false;
+            HasError = false;
+        }
+
+        private async void OnLogin()
+        {
+            IsLoading = true;
+            var authRequest = await _authService.Login(UserModel);
+            UserModel = authRequest;
+
+            if (string.IsNullOrWhiteSpace(UserModel.Token))
+            {
+                IsLoading = false;
+                HasError = true;
+                return;
+            }
+            await Shell.Current.GoToAsync("//ChatPage");
+            IsLoading = false;
+        }
+        public void OnPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new
+                PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- LgpDuvidas/LgpDuvidas/Views/LoginPage.xaml.cs
+++ LgpDuvidas/LgpDuvidas/Views/LoginPage.xaml.cs
@@ -2,6 +2,7 @@
 using LgpDuvidas.Interfaces;
 using LgpDuvidas.Models;
 using LgpDuvidas.Services;
+using LgpDuvidas.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,36 +17,12 @@ namespace LgpDuvidas.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LoginPage : ContentPage
     {
-        public IAuthService _authService => DependencyService.Get<IAuthService>();
-        private AuthModel UserModel { get; set; }
+        LoginPageViewModel vm;
+
         public LoginPage()
         {
             InitializeComponent();
-            UserModel = new AuthModel();
-        }
-
-        private void Login(object sender, EventArgs e)
-        {
-            var authRequest = _authService.Login(UserModel);
-            authRequest.Wait();
-
-            UserModel = authRequest.Result;
-
-            if (string.IsNullOrWhiteSpace(UserModel.Token))
-            {
-
-            }
-            Shell.Current.GoToAsync("//ChatPage").Wait();
-        }
-
-        private void Login_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            UserModel.user = e.NewTextValue;
-        }
-
-        private void Pass_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            UserModel.pass = e.NewTextValue;
+            BindingContext = vm = new LoginPageViewModel();
         }
     }
 }
