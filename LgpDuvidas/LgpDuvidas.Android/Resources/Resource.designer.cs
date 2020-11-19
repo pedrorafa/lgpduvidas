e21a15f692b236b3e4a0007e0f16af667d3408b9
Pedro <pedrorafag00@gmail.com>
2020-11-18 21:54:20 -0300

    resources updates

--- LgpDuvidas/LgpDuvidas.Android/Resources/Resource.designer.cs
+++ LgpDuvidas/LgpDuvidas.Android/Resources/Resource.designer.cs
@@ -8370,19 +8370,16 @@ namespace LgpDuvidas.Droid
 			public const int send = 2131165304;
 			
 			// aapt resource value: 0x7F070079
-			public const int tab_about = 2131165305;
+			public const int tab_feed = 2131165305;
 			
 			// aapt resource value: 0x7F07007A
-			public const int tab_feed = 2131165306;
+			public const int tooltip_frame_dark = 2131165306;
 			
 			// aapt resource value: 0x7F07007B
-			public const int tooltip_frame_dark = 2131165307;
+			public const int tooltip_frame_light = 2131165307;
 			
 			// aapt resource value: 0x7F07007C
-			public const int tooltip_frame_light = 2131165308;
-			
-			// aapt resource value: 0x7F07007D
-			public const int xamarin_logo = 2131165309;
+			public const int xamarin_logo = 2131165308;
 			
 			static Drawable()
 			{
